d0cc9918763b017093e9fd4265bfa952cc26e433
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-04 11:58:35 -0500

    adds file for exercise 8.4.1

---
+++ albums_seeder.sql
@@ -0,0 +1,65 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+
+VALUES ('Michael Jackson', 'Thriller', 1982, 4600, 'Pop, rock, R&B'),
+
+('AC/DC', 'Back in Black', '1980', 2610, 'Hard rock'),
+
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 2420, 'Progressive rock'),
+
+('Whitney Houston / Various artists', 'The Bodyguard', 1992, 2740, 'Soundtrack/R&B, soul, pop'),
+
+('Meat Loaf', 'Bat Out of Hell', 1977, 2060, 'Hard rock, progressive rock'),
+
+('Eagles', 'Their Greatest Hits 1971–1975)', 1976, 3220, 'Rock, soft rock, folk rock'),
+
+('Bee Gees / Various artists', 'Saturday Night Fever', 1977, 2060, 'Disco'),
+
+('Fleetwood Mac', 'Rumours', 1977, 2790, 'Soft rock'),
+
+('Shania Twain', 'Come On Over', 1997, 2960, 'Country, pop'),
+
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 2900, 'Hard rock, heavy metal'),
+
+('Michael Jackson', 'Bad', 1987, 2030, 'Pop, funk, rock'),
+
+('Alanis Morissette', 'Jagged Little Pill', 1995, 2480, 'Alternative rock'),
+
+('Celine Dion', 'Falling into You', 1996, 2020, 'Pop, Soft rock'),
+
+('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 1310, 'Rock'),
+
+('Eagles', 'Hotel California', 1976, 2150, 'Rock, soft rock, folk rock'),
+
+('Mariah Carey', 'Music Box', 1993, 1900, 'Pop, R&B, Rock'),
+
+('Michael Jackson', 'Dangerous', 1991, 1760, 'Rock, Funk, Pop'),
+
+('Various artists', 'Dirty Dancing', 1987, 1790, 'Pop, rock, R&B'),
+
+('Celine Dion', 'Lets Talk About Love', 1997, 1930, 'Pop, Soft rock'),
+
+('The Beatles', '1', 2000, 2160, 'Rock'),
+
+('Adele', '21', 2011, 2230, 'Pop, soul'),
+
+('The Beatles', 'Abbey Road', 1969, 1440, 'Rock'),
+
+('Bruce Springsteen', 'Born in the U.S.A.', 1984, 1960, 'Rock'),
+
+('Dire Straits', 'Brothers in Arms', 1985, 1770, 'Rock'),
+
+('James Horner', 'Titanic: Music from the Motion Picture', 1997, 1810, 'Soundtrack'),
+
+('Madonna', 'The Immaculate Collection', 1990, 1940, 'Pop, Dance'),
+
+('Metallica', 'Metallica', 1991, 1990, 'Thrash metal, heavy metal'),
+
+('Nirvana', 'Nevermind', 1991, 1670, 'Grunge, alternative rock'),
+
+('Pink Floyd', 'The Wall', 1979, 1760, 'Progressive rock'),
+
+('Santana', 'Supernatural', 1999, 2050, 'Rock'),
+
+('Guns N Roses', 'Appetite for Destruction', 1987, 2130, 'Heavy metal, hard rock');
