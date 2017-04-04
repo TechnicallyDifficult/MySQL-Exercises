9c9d4d21cf8f1e6d8da82dee97400b47ba6c4597
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-04 12:00:32 -0500

    adds exercise 8.4.2

---
+++ select_exercise.sql
@@ -0,0 +1,31 @@
+USE codeup_test_db;
+
+SELECT name
+AS 'Albums by Pink Floyd'
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT release_date
+AS 'Year That "Sgt. Peppers Lonely Hearts Club Band" Was Released'
+FROM albums
+WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT genre
+AS 'Genre of "Nevermind"'
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name
+AS 'Albums Released in the 1990s'
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name
+AS 'Albums with Less Than 20 Million Sales'
+FROM albums
+WHERE sales < 2000;
+
+SELECT name
+AS 'Albums in the Rock Genre'
+FROM albums
+WHERE genre = 'Rock';
