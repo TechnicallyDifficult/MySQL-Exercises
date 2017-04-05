e53dd17e7748340481c035fc8119d42799574e24
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-05 14:36:40 -0500

    completes ex 8.7.4

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -9,3 +9,8 @@ FROM employees
 WHERE last_name LIKE 'E%'
   AND last_name LIKE '%e'
 GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
