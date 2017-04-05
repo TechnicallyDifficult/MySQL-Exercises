9ef00771d2f8ce9d75256a232a12e57effd7afd0
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-05 15:46:49 -0500

    completes ex 8.9

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -22,7 +22,8 @@ FROM employees
 WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
   AND birth_date LIKE '%12-25';
 
-SELECT *
+SELECT first_name, last_name, COUNT(*)
 FROM employees
 WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
+  AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name;
